26735c4f085f2d7cdef7768ab53fd41a3d1f3084
HyrGlobalSystem <sasidhareee.reddy@gmail.comn>
2018-10-16 17:10:32 -0500

    Intial commit

---
+++ server/models/Todomodels.js
@@ -0,0 +1,21 @@
+var mongoose=require('mongoose');
+
+var Todo=mongoose.model('Todos',{
+
+    text:{
+      type:String,
+      required:true,
+      minlength:1,
+      trim:true
+    },
+    completed:{
+      type:Boolean,
+      default:false
+    },
+    createdAt:{
+      type:Number,
+      default:null
+    }
+});
+
+module.exports={Todo};
